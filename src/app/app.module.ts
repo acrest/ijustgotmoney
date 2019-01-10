3fc0c982c489f8ba533ca69c35462a9e68cc70f3
Alec <alec.crestani@lovelandinnovations.com>
2019-01-09 22:08:18 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { LandingPage } from "../components/landing-page/landing-page";
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LandingPage
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/components/landing-page/landing-page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "landing-page",
+	templateUrl: "./landing-page.html",
+	styleUrls: ["./landing-page.scss"]
+})
+export class LandingPage implements OnInit
+{
+	private constructor()
+	{
+
+	}
+	public ngOnInit(): void
+	{
+	}
+}
